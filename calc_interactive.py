fdaf51af3cda1e767146d1ae5d177c2b4e1c8ae8
Patric <patricj.timm@gmail.com>
2024-11-22 17:43:16 +0100

    added first interactive graph element

--- calc_interactive.py
+++ calc_interactive.py
@@ -1,14 +1,23 @@
 import streamlit as st
 import calculations
+from calculations import assetprice_change_plot
 
-s = st.number_input("Current Asset Price :",0., value = 100.)
-k = st.number_input("Strike Price :",0., value = 200.)
-r = st.number_input("Interest Rate :",0., value = 0.05)
-vol = st.number_input("Volatility \u03C3 :",0., value = 0.3)
-t = st.number_input("Time to Maturity (in years) :",0., value = 0.75)
+s = st.number_input("Current Asset Price :",0.00001, value = 100.)
+k = st.number_input("Strike Price :",0.00001, value = 200.)
+r = st.number_input("Interest Rate :",0.00001, value = 0.05)
+vol = st.number_input("Volatility \u03C3 :",0.00001, value = 0.3)
+t = st.number_input("Time to Maturity (in years) :",0.00001, value = 0.75)
 
 call_value = calculations.call_value(s,k,r,vol,t)
 put_value = calculations.put_value(s,k,r,vol,t)
 
 st.write(f"Call Value = {call_value}")
 st.write(f"Put Value = {put_value}")
+
+
+start = st.number_input("Start of range: ",0., value = 50.)
+end = st.number_input("End of range: ",0., value = 300.)
+
+fig, ax = assetprice_change_plot(start,end,k,r,vol,t)
+
+st.pyplot(fig)
--- calculations.py
+++ calculations.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import scipy.stats as se
+import matplotlib.pyplot as plt
 
 
 def d1(s,k,r,vol,t):
@@ -17,3 +18,23 @@ def put_value(s,k,r,vol,t):
     val = k * np.exp(-r*t) * se.norm.cdf(-d2(s,k,r,vol,t)) - s * se.norm.cdf(-d1(s,k,r,vol,t))
     return round(val,2)
 
+def assetprice_change_plot(start, end,k,r,vol,t):
+    pricerange = np.linspace(start,end,100)
+    
+    call_prices = [call_value(price,k,r,vol,t) for price in pricerange]
+    put_prices = [put_value(price,k,r,vol,t) for price in pricerange]
+    
+    fig, ax = plt.subplots(figsize = (10,6))
+    
+    ax.plot(pricerange, call_prices, label = "Call Price", color = "blue", linestyle= "-", linewidth=1.5)
+    ax.plot(pricerange, put_prices, label = "Put Price", color = "brown", linestyle = "--", linewidth = 1.5)
+    
+    ax.set_xlabel("Asset Price")
+    ax.set_ylabel("Option Price")
+    
+    ax.set_label("Option Price vs. Asset Price")
+    ax.legend()
+    ax.grid()
+    
+    
+    return fig, ax
